9ab02a5a8632eb7fbdfcd2111e4a47ce9edce18b
Daniel Steman <daniel-steman@live.nl>
2023-07-05 13:58:08 +0000

    add spiders

---
+++ ishares/ishares/spiders/ishares_spider.py
@@ -0,0 +1,57 @@
+import scrapy
+from selenium import webdriver
+from selenium.webdriver.chrome.options import ChromiumOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class IsharesSpider(scrapy.Spider):
+    name = "ishares"
+
+    def start_requests(self):
+        url = "https://www.ishares.com/nl/particuliere-belegger/nl/producten/251781/ishares-euro-stoxx-50-ucits-etf-inc-fund"
+
+        chrome_options = ChromiumOptions()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.get(url)
+
+        accept_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    '//*[@id="onetrust-reject-all-handler"]',
+                )
+            )
+        )
+        accept_button.click()
+
+        continue_as_private_investor_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    '//*[@id="direct-url-screen-{lang}"]/div/div[2]/div/a',
+                )
+            )
+        )
+        continue_as_private_investor_button.click()
+
+        html = driver.page_source
+        driver.quit()
+
+        yield scrapy.Request(url=url, body=html, callback=self.parse)
+
+    def parse(self, response):
+        table_xpath = '//*[@id="allHoldingsTable"]/tbody/tr[1]'
+        element = response.xpath(table_xpath).get()
+        print(f"*********Result: {element}")
+
+        # yield {"table_html": table_html}
+
+        # page = response.url.split("/")[-2]
+        # filename = f"quotes-{page}.html"
+        # Path(filename).write_bytes(response.body)
+        # self.log(f"Saved file {filename}")
