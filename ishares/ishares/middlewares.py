e9bc79ec7eb3ac6da11db3a437a14d423de6df09
danielsteman <daniel-steman@live.nl>
2023-07-07 12:50:50 +0200

    map raw response to class

--- ishares/ishares/middlewares.py
+++ ishares/ishares/middlewares.py
@@ -53,7 +53,7 @@ class IsharesSpiderMiddleware(object):
             yield r
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
 
 
 class IsharesDownloaderMiddleware(object):
@@ -100,4 +100,4 @@ class IsharesDownloaderMiddleware(object):
         pass
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
