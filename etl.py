5cc8fbeee9a16f12d4871a0607a38d95ed474b90
danielsteman <daniel-steman@live.nl>
2023-07-18 23:10:58 +0200

    added print messages

--- etl.py
+++ etl.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
-from enums import ETFManager
-import schemas
 import models
+import schemas
+from enums import ETFManager
 from scrapers import IsharesFundHoldingsScraper, IsharesFundsListScraper
 
 load_dotenv()
@@ -30,10 +30,18 @@ fund_list_scraper = IsharesFundsListScraper(
     "https://www.ishares.com/nl/professionele-belegger/nl/producten/etf-investments#/?productView=all&dataView=keyFactspageNumber=1",  # noqa: E501
     ETFManager.ISHARES,  # type: ignore
 )
+
 fund_list = fund_list_scraper.get_funds_list()
+
+print(f"{len(fund_list)} ishares funds found.")
+
 db = get_db()
 for fund_ref in fund_list:
+    print(f"Processing fund {fund_ref.name}.")
+
     holdings_scraper = IsharesFundHoldingsScraper(fund_ref.url, fund_ref.name)
     holdings = holdings_scraper.get_holdings()
     for holding in holdings:
+        print(f"Processing holding {holding.name}.")
+
         load_holding(db, holding)
